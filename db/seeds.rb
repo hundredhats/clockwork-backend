f74b3659ac4de2f48b04d66d8e4730e94d93db3d
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2018-03-26 23:40:04 -0400

    Add a couple projects to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+["Project 1", "Project 2"].each do |name|
+  Project.create!(name: name)
+end
